d5e55a5230ddc045fddb0d95a34514333b71125b
aso1501162 <aso1501162@st.asojuku.ac.jp>
2017-06-22 11:59:20 +0900

    管理者リストと科目分類リストのDAO

---
+++ seminar/src/dao/CategoryDAO.java
@@ -0,0 +1,85 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+import model.Category;
+
+public class CategoryDAO {
+	// データソース
+	DataSource ds = null;
+	// データベース接続情報
+	Connection con = null;
+	// プリコンパイル用のステートメント
+	PreparedStatement stmt = null;
+	// SELECTの結果を格納するResultSet
+	ResultSet rs = null;
+
+	// データベース接続
+	public Connection connection() throws Exception {
+		// データソースがなければ、context.xmlから読み込んで設定する
+		if (ds == null) {
+			ds = (DataSource) (new InitialContext()).lookup("java:comp/env/jdbc/MySQL");
+		}
+		con = ds.getConnection();
+		return con;
+	}
+
+	// データベース切断
+	public void close() throws Exception {
+		// データベース接続されていれば、切断する
+		if (rs != null) {
+			rs.close();
+		}
+		if (stmt != null) {
+			stmt.close();
+		}
+		if (con != null) {
+			con.close();
+		}
+	}
+	
+	// 科目分類リストの取得
+public ArrayList<Category> getCategoryList() {
+		
+		ArrayList<Category> categoryList = new ArrayList<Category>();
+		
+		try {
+			// DB接続
+			connection();
+
+			// SQL文設定の準備・SQL文の実行
+			String sql =
+					"SELECT * "
+					+ "FROM category";
+			stmt = con.prepareStatement(sql);
+			rs = stmt.executeQuery(); // sql文を実行
+			
+			System.out.println("---------------------------------");
+			
+			while (rs.next()) {
+				Category category = new Category();
+				
+				System.out.println("科目分類："+rs.getString("category_name") + "を取得");
+
+				category.setCategoryId(rs.getInt("category_id"));
+				category.setCategoryName(rs.getString("category_name"));
+
+				categoryList.add(category);
+			}
+		} catch (Exception e) {
+		} finally {
+			try {
+				close();
+			} catch (Exception e) {
+
+			}
+		}
+		return categoryList;
+	}
+}
