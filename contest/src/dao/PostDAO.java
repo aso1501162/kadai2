6174ee8f81e7d7fc987778a82fe36204315488bb
aso1501162 <aso1501162@st.asojuku.ac.jp>
2017-07-19 15:07:02 +0900

    postIdから投稿ひとつを取得するメソッドを追加

--- contest/src/dao/PostDAO.java
+++ contest/src/dao/PostDAO.java
@@ -6,6 +6,44 @@ import model.Post;
 
 public class PostDAO extends CommonDAO {
 	
+	//	投稿取得
+	public Post getPost(int postId) {
+		System.out.println("---------------投稿取得---------------");
+		Post post = new Post();
+		
+		try {
+			// DB接続
+			connection();
+
+			// SQL文設定の準備・SQL文の実行
+			String sql =
+					"SELECT * "
+					+ "FROM post "
+					+ "WHERE post_id = ?";
+			stmt = con.prepareStatement(sql);
+			stmt.setInt(1, postId);
+			rs = stmt.executeQuery(); // sql文を実行
+			
+				System.out.println("投稿：" + rs.getString("title") + " を取得");
+
+				post.setPostId(rs.getInt("post_id"));
+				post.setFileName(rs.getString("file_name"));
+				post.setTitle(rs.getString("title"));
+				post.setCommentList(new CommentDAO().getCommentList(rs.getInt("post_id")));
+				post.setVotes(new VoteDAO().getVotes(rs.getInt("post_id")));
+
+		} catch (Exception e) {
+			System.out.println(e);
+		} finally {
+			try {
+				close();
+			} catch (Exception e) {
+
+			}
+		}
+		return post;
+	}
+	
 	//	投稿リスト取得
 	public ArrayList<Post> getPostList() {
 		System.out.println("---------------投稿リスト取得---------------");
@@ -18,8 +56,7 @@ public class PostDAO extends CommonDAO {
 			// SQL文設定の準備・SQL文の実行
 			String sql =
 					"SELECT * "
-					+ "FROM post "
-					+ "ORDER BY subject_id";
+					+ "FROM post";
 			stmt = con.prepareStatement(sql);
 			rs = stmt.executeQuery(); // sql文を実行
 			
